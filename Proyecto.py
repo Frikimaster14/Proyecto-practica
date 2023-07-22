5480a8d9fd9ad8cd42ccbe99a0e44989bb03817b
Frikimaster14 <140205680+Frikimaster14@users.noreply.github.com>
2023-07-21 21:31:36 -0300

    Create Proyecto.py

---
+++ Proyecto.py
@@ -0,0 +1,9 @@
+def main():
+ # Solicitar al usuario que ingrese su nombre
+ nombre = input("Por favor, ingresa tu nombre: ")
+
+ # Mostrar un mensaje de bienvenida
+ print("¡Hola," ,nombre + "¡ Bienvenido a python.")
+
+if __name__ == "__main__":
+ main()
